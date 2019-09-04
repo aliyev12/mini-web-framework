f4dadf28cd5a21fbef68ebdfb2905da315e1c22a
Abdul Aliyev <aliyev_abdul@bah.com>
2019-09-03 20:33:37 -0400

    Add UserEdit

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -5,9 +5,14 @@ export class UserForm extends View<User, UserProps> {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:button.set-age': this.onSetAgeClick,
-      'click:button.set-name': this.onSetNameClick
+      'click:button.set-name': this.onSetNameClick,
+      'click:button.save-model': this.onSaveClick,
     };
   }
+
+  onSaveClick = (): void => {
+    this.model.save();
+  };
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
@@ -25,12 +30,10 @@ export class UserForm extends View<User, UserProps> {
   template(): string {
     return `
             <div>
-                <h1>User Form</h1>
-                <div>User Name: ${this.model.get('name')}</div>
-                <div>User Age: ${this.model.get('age')}</div>
-                <input />
+                <input placeholder="${this.model.get('name')}" />
                 <button class="set-name">Change Name</button>
                 <button class="set-age">Set Random Age</button>
+                <button class="save-model">Save User</button>
             </div>
         `;
   }
